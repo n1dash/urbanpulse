b2888b1a33901dc718c22a21fe3f3d46043446fb
Sarvesharu <sarveshaapshette@gmail.com>
2026-07-27 10:21:51 +0530

    Add Docker support for UrbanPulse

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173,
-    host: true
+export default defineConfig(({ mode }) => {
+  console.log("✅ Vite config loaded");
+
+  return {
+    plugins: [react()],
+    server: {
+      host: '0.0.0.0',
+      port: 5173,
+      allowedHosts: [
+        '.trycloudflare.com'
+      ]
+    }
   }
 })
